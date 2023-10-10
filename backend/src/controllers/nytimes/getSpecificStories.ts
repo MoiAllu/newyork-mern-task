fbc0ca3c3c0f8abcc69363a5becddd5f1623ba6b
Ali Abbasi <al5690410@gmail.com>
2023-10-11 02:54:13 +0500

    chore: get Specific stoires

---
+++ backend/src/controllers/nytimes/getSpecificStories.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from 'express';
+
+/**
+ 
+Health check endpoint*/
+const getSpecificStories: RequestHandler = async (req, res) => {
+  try {
+    const { section } = req.params;
+    console.log(section);
+    const sections = ['arts', 'home', 'science', 'us', 'world'];
+
+    if (!sections.includes(section.toLowerCase())) {
+      return res.status(400).json({
+        error: `Invalid section. Valid sections are: ${sections.join(', ')}`,
+      });
+    }
+
+    const url = `https://api.nytimes.com/svc/topstories/v2/${section.toLowerCase()}.json?api-key=gsXg80672WIn26CuAAHvLmxTNyrqKDCK`
+
+    const response = await fetch(url);
+
+    const json = await response.json();
+
+    return res.status(200).json(json);
+  } catch (error:any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export default getSpecificStories;
--- backend/src/routes/nytimes.ts
+++ backend/src/routes/nytimes.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import getStories from '../controllers/nytimes/getStories';
+import getSpecificStories from '../controllers/nytimes/getSpecificStories';
 const root = express.Router();
 
 root.get('/', getStories);
+root.get('/topstories/:section', getSpecificStories);
 
 export default root;
